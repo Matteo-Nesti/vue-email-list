875392e98ee9527b47b9bb841c51c5cd3eefe172
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-06-27 15:24:42 +0200

    Add mails generator

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+const link = "https://flynn.boolean.careers/exercises/api/random/mail";
+
 const { createApp } = Vue;
 
 const app = createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      items: 10,
+      mails: [],
     };
+  },
+  created() {
+    axios.get(link).then((res) => {
+      for (let i = 0; i < this.items; i++) {
+        this.mails.push(res.data.response);
+      }
+    });
   },
 }).mount("#root");
